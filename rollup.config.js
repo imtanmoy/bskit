c4301826dac137014cebfb07b0775d167c739ea6
Tanmoy Banik <Tanmoy Banik>
2021-01-08 12:20:17 +0600

    rollup setup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+import scss from 'rollup-plugin-scss';
+
+import packageJson from './package.json';
+
+export default {
+  input: './src/index.tsx',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      exports: 'named',
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), scss()],
+};
